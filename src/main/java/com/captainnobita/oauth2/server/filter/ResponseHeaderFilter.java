06ea00496e263c40d5591b69a905258640af62a2
huynx <huynx@napas.com.vn>
2025-03-01 15:59:12 +0700

    Bổ sung các loại log

--- src/main/java/com/captainnobita/oauth2/server/filter/ResponseHeaderFilter.java
+++ src/main/java/com/captainnobita/oauth2/server/filter/ResponseHeaderFilter.java
@@ -4,17 +4,21 @@
  */
 package com.captainnobita.oauth2.server.filter;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 /**
  *
@@ -23,22 +27,43 @@ import org.springframework.web.filter.GenericFilterBean;
 @Component
 @Slf4j
 public class ResponseHeaderFilter extends GenericFilterBean {
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain fc) throws IOException, ServletException {
 
-        ServletResponseWrapperCopier capturingResponseWrapper = new ServletResponseWrapperCopier(
-                (HttpServletResponse) response);
+        HttpServletResponse httpServeletResponse = (HttpServletResponse) response;
+        
+        HttpServletRequest httpServeletRequest = (HttpServletRequest)request;
+        String uri = httpServeletRequest.getRequestURI(); 
+        
+        Enumeration<String> headerNames = httpServeletRequest.getHeaderNames();
+        List<String> listHeader = new ArrayList<>();
+        
+        // Kiểm tra nếu có header
+        if (httpServeletRequest != null) {
+            // Duyệt qua từng header và log thông tin
+            while (headerNames.hasMoreElements()) {
+                String headerName = headerNames.nextElement();
+                String headerValue = httpServeletRequest.getHeader(headerName);
+                listHeader.add(headerName + ": " + headerValue);
+            }
+        }
+        
+        log.info("Uri:{} headers:[{}]", uri, String.join(", ", listHeader));
+        
+        // Wrap the request and response to capture content
+        ContentCachingRequestWrapper wrappedRequest = new ContentCachingRequestWrapper(httpServeletRequest);
+        ContentCachingResponseWrapper wrappedResponse = new ContentCachingResponseWrapper(httpServeletResponse);
 
         try {
-            fc.doFilter(request, capturingResponseWrapper);
+            fc.doFilter(wrappedRequest, wrappedResponse);
         } finally {
-            String responseBody = capturingResponseWrapper.getCaptureAsString();
-            log.info("Tra ve response:{}", responseBody);
-            response.getOutputStream().write(responseBody.getBytes());
+            String requestBody = new String(wrappedRequest.getContentAsByteArray());
+            String responseBody = new String(wrappedResponse.getContentAsByteArray());
+            log.info("Request URI:{} httpCode:{} requestBody:{} responseBody:{}", 
+                    uri, httpServeletResponse.getStatus(), requestBody, responseBody);
+            
+            // Giữ lại dữ liệu trong response để trả về cho client
+            wrappedResponse.copyBodyToResponse();
         }
     }
 }
